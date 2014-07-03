fa91cc2b8a6eed7f76cbb7f84b31b7ceab45e58f
Konrad Lewandowski <korrix@gmail.com>
2014-07-03 11:15:38 +0000

    Grid is now storable on accelerate

--- src/Math/Space/Space.hs
+++ src/Math/Space/Space.hs
@@ -4,8 +4,27 @@
 -- Proprietary and confidential
 -- Flowbox Team <contact@flowbox.io>, 2014
 ---------------------------------------------------------------------------
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Math.Space.Space where
+
+import           Data.Array.Accelerate
+import           Data.Array.Accelerate.Smart
+import           Data.Array.Accelerate.Tuple
+import           Data.Array.Accelerate.Array.Sugar
+import           Data.Complex
+import qualified Data.Foldable as F
+import           Data.Typeable
+
+import Control.Applicative
 
 --data Space2 = Space2 !Double !Double deriving (Show)
 
@@ -16,8 +35,73 @@ class Space2 space where
 class Space2 space => Space3 space where
 	depth :: space a -> a
 
-data Grid a = Grid !a !a deriving (Show)
+data Grid a = Grid !a !a deriving (Eq, Ord, Show, Read,Typeable)
 
 instance Space2 Grid where
 	width  (Grid x _) = x
 	height (Grid _ x) = x
+
+--------------------------------------------------------------------------------
+-- Grid
+--------------------------------------------------------------------------------
+instance Functor Grid where
+    fmap f (Grid a b) = Grid (f a) (f b)
+
+instance Applicative Grid where
+    pure a = Grid a a
+    {-# INLINE pure #-}
+    Grid a b <*> Grid d e = Grid (a d) (b e)
+    {-# INLINE (<*>) #-}
+
+instance Num a => Num (Grid a) where
+    (+) = liftA2 (+)
+    {-# INLINE (+) #-}
+    (-) = liftA2 (-)
+    {-# INLINE (-) #-}
+    (*) = liftA2 (*)
+    {-# INLINE (*) #-}
+    negate = fmap negate
+    {-# INLINE negate #-}
+    abs = fmap abs
+    {-# INLINE abs #-}
+    signum = fmap signum
+    {-# INLINE signum #-}
+    fromInteger = pure . fromInteger
+    {-# INLINE fromInteger #-}
+
+instance Fractional a => Fractional (Grid a) where
+    recip = fmap recip
+    {-# INLINE recip #-}
+    (/) = liftA2 (/)
+    {-# INLINE (/) #-}
+    fromRational = pure . fromRational
+    {-# INLINE fromRational #-}
+
+type instance EltRepr (Grid a)  = EltRepr (a, a)
+type instance EltRepr' (Grid a) = EltRepr' (a, a)
+
+instance Elt a => Elt (Grid a) where
+  eltType _ = eltType (undefined :: (a,a))
+  toElt p = case toElt p of
+     (x, y) -> Grid x y
+  fromElt (Grid x y) = fromElt (x, y)
+
+  eltType' _ = eltType' (undefined :: (a,a))
+  toElt' p = case toElt' p of
+     (x, y) -> Grid x y
+  fromElt' (Grid x y) = fromElt' (x, y)
+
+instance IsTuple (Grid a) where
+  type TupleRepr (Grid a) = TupleRepr (a,a)
+  fromTuple (Grid x y) = fromTuple (x,y)
+  toTuple t = case toTuple t of
+     (x, y) -> Grid x y
+
+instance (Lift Exp a, Elt (Plain a)) => Lift Exp (Grid a) where
+  type Plain (Grid a) = Grid (Plain a)
+  --lift = Exp . Tuple . F.foldl SnocTup NilTup
+  lift (Grid x y) = Exp $ Tuple $ NilTup `SnocTup` lift x `SnocTup` lift y
+
+instance (Elt a, e ~ Exp a) => Unlift Exp (Grid e) where
+  unlift t = Grid (Exp $ SuccTupIdx ZeroTupIdx `Prj` t)
+                (Exp $ ZeroTupIdx `Prj` t)
